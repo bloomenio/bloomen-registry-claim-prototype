ba30cdef6b65f29d36bfcfe0bb7ebfbae811ea3f
Raul Fernández <raul.fernandezgarcia@worldline.com>
2018-10-02 10:42:34 +0200

    Demo2Task & Demo2Claim

--- ethereum/contracts/Demo2Claim.sol
+++ ethereum/contracts/Demo2Claim.sol
@@ -1,7 +1,39 @@
 pragma solidity ^0.4.22;
 
 import "./Ownable.sol";
+import "./SafeMath.sol";
 
 contract Demo2Claim is Ownable {
 
+    using SafeMath for uint256;
+
+    event ClaimCreated(uint256 _assetId, address _assetOwner, string _description, uint256 _id, uint256 _claimId, address _claimOwner);
+    event ClaimUpdated(uint256 _assetId, address _assetOwner, string _description);
+
+    struct Claim {
+        uint256 assetId;
+        address assetOwner;
+        string description;
+        uint256 id;
+        uint256 claimId;
+        address claimOwner;
+    }
+
+    mapping (uint256 => Claim) public claims;
+    uint256 private numClaims;
+
+    function createClaim(uint256 _assetId, address _assetOwner, string _description, uint256 _id) public onlyOwner {
+        uint256 claimId = numClaims.add(1);
+        claims[claimId] = Claim(_assetId, _assetOwner, _description, _id, claimId, msg.sender);
+        emit ClaimCreated(_assetId, _assetOwner, _description, _id, claimId, msg.sender);
+    }
+
+    function updateClaim(uint256 _claimId, uint256 _assetId, address _assetOwner, string _description) public onlyOwner {
+        require(_claimId > 0);
+        Claim storage claim = claims[_claimId];
+        claim.assetId = _assetId;
+        claim.assetOwner = _assetOwner;
+        claim.description = _description;
+        emit ClaimUpdated(claim.assetId, claim.assetOwner, claim.description);
+    }
 }
--- ethereum/contracts/Demo2Task.sol
+++ ethereum/contracts/Demo2Task.sol
@@ -1,7 +1,39 @@
 pragma solidity ^0.4.22;
 
 import "./Ownable.sol";
+import "./SafeMath.sol";
 
 contract Demo2Task is Ownable {
     
+    using SafeMath for uint256;
+
+    event TaskCreated(string description, address to, uint256 issueId, uint256 claimId, address claimOwner, address from);
+    event TaskUpdated(string description, address to);
+
+    struct Task {
+        string description;
+        address to;
+        uint256 issueId;
+        uint256 claimId;
+        address claimOwner;
+        address from;
+    }
+
+    mapping (uint256 => Task) public tasks;
+    uint256 private numTasks;
+
+    function createTask(string _description, address _to, uint256 _issueId, uint256 _claimId, address _claimOwner) public onlyOwner {
+        uint256 taskId = numTasks.add(1);
+        tasks[taskId] = Task(_description, _to, _issueId, _claimId, _claimOwner, msg.sender);
+        emit TaskCreated(_description, _to, _issueId, _claimId, _claimOwner, msg.sender);
+    }
+
+    function updateTask(uint256 _taskId, string _description, address _to) public onlyOwner {
+        require(_taskId > 0);
+        Task storage task = tasks[_taskId];
+        task.description = _description;
+        task.to = _to;
+        emit TaskUpdated(task.description, task.to);
+    }
+
 }
